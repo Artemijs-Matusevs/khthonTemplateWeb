f77273592d4bf910caa5f4f3990c4e5e101855ab
Artemijs-Matusevs <126737489+Artemijs-Matusevs@users.noreply.github.com>
2023-08-24 02:01:45 +0100

    Add files via upload

--- public/js/page.js
+++ public/js/page.js
@@ -6,12 +6,13 @@ function rotateBackground(){
     document.getElementById("indexNav").style.opacity = "1";
     document.getElementById("indexNav").style.pointerEvents = "all";
 
-    document.getElementById("menuButton").style.display = "none";
-    document.getElementById("menuButton2").style.display = "block";
+    document.getElementById("menuButton").style.transform = 'rotate(180deg) scale(1.2)';
+    document.getElementById("menuButton2").style.transform = 'rotate(180deg) scale(1.2)';
+
+
+    setTimeout(switchMenuButtons, 500);
 
     document.getElementById("banner-info").style.opacity = "0";
-
-
 }
 
 function rotateBackgroundBack(){
@@ -20,8 +21,21 @@ function rotateBackgroundBack(){
     document.getElementById("indexNav").style.opacity = "0";
     document.getElementById("indexNav").style.pointerEvents = "none";
 
-    document.getElementById("menuButton").style.display = "block";
-    document.getElementById("menuButton2").style.display = "none";
+    setTimeout(switchMenuButtons2, 500);
 
     document.getElementById("banner-info").style.opacity = "1";
+
+    document.getElementById("menuButton").style.transform = 'rotate(0deg)';
+    document.getElementById("menuButton2").style.transform = 'rotate(0deg)';
+}
+
+
+function switchMenuButtons(){
+    document.getElementById("menuButton").style.display = "none";
+    document.getElementById("menuButton2").style.display = "block";
+}
+
+function switchMenuButtons2(){
+    document.getElementById("menuButton").style.display = "block";
+    document.getElementById("menuButton2").style.display = "none";
 }
